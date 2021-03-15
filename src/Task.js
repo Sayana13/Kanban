2be570a876332a41ab8cf3f26910ffe36912e94a
Sayana13 <domshoevas@gmail.com>
2021-03-14 21:12:09 -0700

    Finished

--- src/Task.js
+++ src/Task.js
@@ -10,7 +10,7 @@ function Task(props) {
             <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
             <p className="card-text">Some action</p>
 
-            {props.task.status !== 'backlogs' &&
+            {props.task.status !== 'backlog' && props.task.status !== 'done' &&
             <button
                 type="button"
                 className="btn btn-link"
